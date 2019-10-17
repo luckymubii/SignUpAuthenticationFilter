523c682db0a87ad54b780a5dd8260a3a818d51a0
HP <bse163056@cust.pk>
2019-10-17 22:11:52 +0500

    Sign Up servlet

---
+++ src/servlets/SignUpServlet.java
@@ -0,0 +1,43 @@
+package servlets;
+
+import Db.DbConnection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "SignUpServlet")
+public class SignUpServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String firstname = request.getParameter("firstname");
+        String lastname = request.getParameter("lastname");
+        String phonenumber = request.getParameter("phonenumber");
+        String email = request.getParameter("email");
+        String addressline1 = request.getParameter("addressline1");
+        String addressline2 = request.getParameter("addressline2");
+        String city = request.getParameter("city");
+        String state = request.getParameter("state");
+        String zip = request.getParameter("zipcode");
+       int zipcode=Integer.parseInt(zip);
+        PrintWriter out = response.getWriter();
+        DbConnection obj = new DbConnection();
+
+        obj.insertData(firstname,lastname,phonenumber,email,addressline1,addressline2,city,state,zipcode);
+
+
+
+
+        out.write("Record Saved");
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
